b52f1b22ff85ac9aa2c09ebca74b62b391870f0f
GOWTHAM MCA <gowthammohan342@gmail.com>
2026-07-22 13:08:33 +0530

    feat : old files deleted

--- wp-content/updraft/plugins-old/secure-custom-fields/assets/build/js/acf.min.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'c9d898d748096d5c9a16');
--- wp-content/updraft/plugins-old/secure-custom-fields/assets/build/js/pro/acf-pro-input.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'f539d190d855ad0b1f59');

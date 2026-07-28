fead7bd2174a363599a35faaf54d0c5175d2a901
GOWTHAM MCA <gowthammohan342@gmail.com>
2026-07-28 22:03:05 +0530

    feat : livebackup updated

--- wp-content/updraft/plugins-old/secure-custom-fields/assets/build/js/pro/acf-field-bindings.min.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'cdce69d494f90b1276e3');
--- wp-content/updraft/plugins-old/secure-custom-fields/assets/build/js/pro/acf-pro-input.min.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'fced8009aa8fffbf7ca9');
